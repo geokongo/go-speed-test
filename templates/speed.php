2bf9dc9eb20bfcbd621460ad6bbf32ad0268e0ec
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2023-02-28 15:11:41 +0300

    Creating the template file for displaying the speed test results

---
+++ templates/speed.php
@@ -0,0 +1,18 @@
+<style>
+.entry-title, .wp-block-post-title {
+    display: none;
+}
+</style>
+<div class="main-speedtest-container">
+    <div class="speedtest-container">
+        <div class="speedtest-loader-widget">
+            <h1>CHECK SPEED</h1>
+            <span class="speedtest-loader speedtest-hide"></span>
+            <div class="speedtest-loader-content">
+                <div class="speedtest-content speedtest-hide">24<small>Mbps</small></div>
+                <button id="speedtest-button">GO</button>
+            </div>
+        </div>
+    </div>
+</div>
+        
